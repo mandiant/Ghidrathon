47e343f0dfe29c53eaf2591729cbf099874baa77
Mike Hunhoff <mike.hunhoff@gmail.com>
2023-04-25 11:03:39 -0600

    use google-java-format to format Java files (#42)

--- src/main/java/ghidrathon/GhidrathonConfig.java
+++ src/main/java/ghidrathon/GhidrathonConfig.java
@@ -3,83 +3,83 @@
 //  you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: [package root]/LICENSE.txt
 // Unless required by applicable law or agreed to in writing, software distributed under the License
-//  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and limitations under the License.
+//  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.
+// See the License for the specific language governing permissions and limitations under the
+// License.
 
 package ghidrathon;
 
-import java.util.List;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Ghidrathon's configuration class
  *
- * Stores
- *   - stdout and stderr
- *   - Python modules to handle as shared modules - relevant to CPython modules
- *   - Java package names to exclude from Python imports
- *   - Python include paths to add to Python interpreter environment
+ * <p>Stores - stdout and stderr - Python modules to handle as shared modules - relevant to CPython
+ * modules - Java package names to exclude from Python imports - Python include paths to add to
+ * Python interpreter environment
  */
 public class GhidrathonConfig {
 
-	private final List<String> javaExcludeLibs = new ArrayList<String>();
-	private final List<String> pyIncludePaths = new ArrayList<String>();
-	private final List<String> pySharedModules = new ArrayList<String>();
+  private final List<String> javaExcludeLibs = new ArrayList<String>();
+  private final List<String> pyIncludePaths = new ArrayList<String>();
+  private final List<String> pySharedModules = new ArrayList<String>();
 
-	private PrintWriter out = null;
-	private PrintWriter err = null;
+  private PrintWriter out = null;
+  private PrintWriter err = null;
 
-	public void addStdOut(PrintWriter out) {
-		this.out = out;
-	}
+  public void addStdOut(PrintWriter out) {
+    this.out = out;
+  }
 
-	public void addStdErr(PrintWriter err) {
-		this.err = err;
-	}
+  public void addStdErr(PrintWriter err) {
+    this.err = err;
+  }
 
-	public PrintWriter getStdOut() {
-		return out;
-	}
+  public PrintWriter getStdOut() {
+    return out;
+  }
 
-	public PrintWriter getStdErr() {
-		return err;
-	}
+  public PrintWriter getStdErr() {
+    return err;
+  }
 
-	public void addPythonSharedModule(String name) {
-		pySharedModules.add(name);
-	}
+  public void addPythonSharedModule(String name) {
+    pySharedModules.add(name);
+  }
 
-	public void addPythonSharedModules(List<String> names) {
-		pySharedModules.addAll(names);
-	}
+  public void addPythonSharedModules(List<String> names) {
+    pySharedModules.addAll(names);
+  }
 
-	public Iterable<String> getPythonSharedModules() {
-		return Collections.unmodifiableList(pySharedModules);
-	}
+  public Iterable<String> getPythonSharedModules() {
+    return Collections.unmodifiableList(pySharedModules);
+  }
 
-	public void addJavaExcludeLib(String name) {
-		javaExcludeLibs.add(name);
-	}
+  public void addJavaExcludeLib(String name) {
+    javaExcludeLibs.add(name);
+  }
 
-	public void addJavaExcludeLibs(List<String> names) {
-		javaExcludeLibs.addAll(names);
-	}
+  public void addJavaExcludeLibs(List<String> names) {
+    javaExcludeLibs.addAll(names);
+  }
 
-	public Iterable<String> getJavaExcludeLibs() {
-		return Collections.unmodifiableList(javaExcludeLibs);
-	}
+  public Iterable<String> getJavaExcludeLibs() {
+    return Collections.unmodifiableList(javaExcludeLibs);
+  }
 
-	public void addPythonIncludePath(String path) {
-		pyIncludePaths.add(path);
-	}
+  public void addPythonIncludePath(String path) {
+    pyIncludePaths.add(path);
+  }
 
-	public void addPythonIncludePaths(List<String> paths) {
-		pyIncludePaths.addAll(paths);
-	}
+  public void addPythonIncludePaths(List<String> paths) {
+    pyIncludePaths.addAll(paths);
+  }
 
-	public Iterable<String> getPythonIncludePaths() {
-		return Collections.unmodifiableList(pyIncludePaths);
-	}
+  public Iterable<String> getPythonIncludePaths() {
+    return Collections.unmodifiableList(pyIncludePaths);
+  }
 }
--- src/main/java/ghidrathon/GhidrathonScriptProvider.java
+++ src/main/java/ghidrathon/GhidrathonScriptProvider.java
@@ -3,91 +3,86 @@
 //  you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: [package root]/LICENSE.txt
 // Unless required by applicable law or agreed to in writing, software distributed under the License
-//  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and limitations under the License.
+//  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.
+// See the License for the specific language governing permissions and limitations under the
+// License.
 
 package ghidrathon;
 
+import generic.jar.ResourceFile;
+import ghidra.app.script.GhidraScript;
+import ghidra.app.script.GhidraScriptLoadException;
+import ghidra.app.script.GhidraScriptProvider;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import generic.jar.ResourceFile;
-
-import ghidra.app.script.GhidraScript;
-import ghidra.app.script.GhidraScriptProvider;
-import ghidra.app.script.GhidraScriptLoadException;
-
 public class GhidrathonScriptProvider extends GhidraScriptProvider {
 
-	@Override
-	public String getDescription() {
-		
-		return "Python 3";
-		
-	}
+  @Override
+  public String getDescription() {
 
-	@Override
-	public String getExtension() {
-		
-		return ".py";
-		
-	}
+    return "Python 3";
+  }
 
-	@Override
-	public GhidraScript getScriptInstance(ResourceFile sourceFile, PrintWriter writer)
-			throws GhidraScriptLoadException {
-		try{
-			GhidraScript script;
-			script = GhidrathonScript.class.getDeclaredConstructor().newInstance();
-			script.setSourceFile(sourceFile);
+  @Override
+  public String getExtension() {
 
-			return script;
-		}
-		catch (ReflectiveOperationException e) {
-			throw new GhidraScriptLoadException("Unable to instantiate: " + e.getMessage(), e);
-		}
-	}
+    return ".py";
+  }
 
-	@Override
-	public void createNewScript(ResourceFile newScript, String category) throws IOException {
-		
-		PrintWriter writer = new PrintWriter(new FileWriter(newScript.getFile(false)));
-		
-		writeHeader(writer, category);
-		writer.println("");
-		writeBody(writer);
-		writer.println("");
-		writer.close();
-		
-	}
+  @Override
+  public GhidraScript getScriptInstance(ResourceFile sourceFile, PrintWriter writer)
+      throws GhidraScriptLoadException {
+    try {
+      GhidraScript script;
+      script = GhidrathonScript.class.getDeclaredConstructor().newInstance();
+      script.setSourceFile(sourceFile);
 
-	@Override
-	public String getCommentCharacter() {
-		
-		return "#";
-		
-	}
-	
-	/**
-	 * Commandeer the .py script extension
-	 * 
-	 * Ghidra loads script providers in order determined by Collections.sort; Ghidra then selects the first script provider that accepts the file extension of the script to be executed
-	 * 		see https://github.com/NationalSecurityAgency/ghidra/blob/8b2ea61e27c07c48dc21eff9095905f739208703/Ghidra/Features/Base/src/main/java/ghidra/app/script/GhidraScriptUtil.java#L274-L281
-	 * 
-	 * Collections.sort invokes GhidraScriptProvider.compareTo so we can override compareTo and check if the script provider we are being compared to uses the .py extension; if true, we simply return
-	 * -1 to be ordered higher in the list of script providers used by Ghidra
-	 */
-	@Override
-	public int compareTo(GhidraScriptProvider that) {
-		
-		if (that.getExtension().equals(".py")) {
-			// return -1 so our script provider is preferred
-			return -1;
-		}
-		
-		return super.compareTo(that);
-		
-	}
+      return script;
+    } catch (ReflectiveOperationException e) {
+      throw new GhidraScriptLoadException("Unable to instantiate: " + e.getMessage(), e);
+    }
+  }
 
+  @Override
+  public void createNewScript(ResourceFile newScript, String category) throws IOException {
+
+    PrintWriter writer = new PrintWriter(new FileWriter(newScript.getFile(false)));
+
+    writeHeader(writer, category);
+    writer.println("");
+    writeBody(writer);
+    writer.println("");
+    writer.close();
+  }
+
+  @Override
+  public String getCommentCharacter() {
+
+    return "#";
+  }
+
+  /**
+   * Commandeer the .py script extension
+   *
+   * <p>Ghidra loads script providers in order determined by Collections.sort; Ghidra then selects
+   * the first script provider that accepts the file extension of the script to be executed see
+   * https://github.com/NationalSecurityAgency/ghidra/blob/8b2ea61e27c07c48dc21eff9095905f739208703/Ghidra/Features/Base/src/main/java/ghidra/app/script/GhidraScriptUtil.java#L274-L281
+   *
+   * <p>Collections.sort invokes GhidraScriptProvider.compareTo so we can override compareTo and
+   * check if the script provider we are being compared to uses the .py extension; if true, we
+   * simply return -1 to be ordered higher in the list of script providers used by Ghidra
+   */
+  @Override
+  public int compareTo(GhidraScriptProvider that) {
+
+    if (that.getExtension().equals(".py")) {
+      // return -1 so our script provider is preferred
+      return -1;
+    }
+
+    return super.compareTo(that);
+  }
 }
