1ac1949da26311f91baa3b7cd3400a654bd63c32
Mike Hunhoff <mike.hunhoff@gmail.com>
2024-01-30 11:45:29 -0700

    fix imports

--- util/ghidrathon_configure.py
+++ util/ghidrathon_configure.py
@@ -6,7 +6,7 @@
 #  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-import importlib
+import importlib.util
 import argparse
 import pathlib
 import logging
